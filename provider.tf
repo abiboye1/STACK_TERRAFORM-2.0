79e412fe555b90ffe591f95ac371b29967fcc2ef
abiboye1 <yomioye007@gmail.com>
2024-04-02 03:25:21 -0400

    Modifying Provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,6 @@
 provider "aws" {
-  region = local.db_creds.AWS_REGION
+  # region = local.db_creds.AWS_REGION
+  region = "us-east-1"
 
 assume_role {
   #the role ARN within Account A to assume role into. Created in step 1
